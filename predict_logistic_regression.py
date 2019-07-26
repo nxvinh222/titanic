4870fd2f87175afd91ab73b92b3248ab99daf66f
unknown <nxvinh222@gmail.com>
2019-07-26 11:54:06 +0700

    .

--- predict_logistic_regression.py
+++ predict_logistic_regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import utils
-from sklearn import linear_model
+from sklearn import linear_model, preprocessing
 
 train = pd.read_csv("train.csv")
 utils.clean_data(train)
@@ -12,3 +12,9 @@ classifier = linear_model.LogisticRegression()
 classifier_ = classifier.fit(features, target)
 
 print(classifier.fit(features, target).score(features, target))
+
+poly = preprocessing.PolynomialFeatures(degree=2)
+poly_features = poly.fit_transform(features)
+
+classifier_ = classifier.fit(poly_features, target)
+print(classifier_.score(poly_features, target))
